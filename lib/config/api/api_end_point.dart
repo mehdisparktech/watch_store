efbe4d13be91958d9ec5450c12d58bad0047e24c
ird-mehdi <mehdidev.ird@gmail.com>
2025-08-13 15:31:46 +0600

    api

--- lib/config/api/api_end_point.dart
+++ lib/config/api/api_end_point.dart
@@ -1,7 +1,7 @@
 class ApiEndPoint {
-  static const baseUrl = "http://103.145.138.74:3000/api/v1";
-  static const imageUrl = "http://103.145.138.74:3000";
-  static const socketUrl = "http://103.145.138.74:3001";
+  static const baseUrl = "http://192.168.10.207:5003/api/v1";
+  static const imageUrl = "http://192.168.10.207:5003";
+  static const socketUrl = "http://192.168.10.207:5003";
 
   static const signUp = "users/sign-up";
   static const verifyEmail = "users/verify-email";
